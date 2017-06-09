620319ff50bf9db08ad29b446feb55d5af93d575
PTKhuong96 <phankhuong96@gmail.com>
2017-06-09 07:41:01 +0700

    common template

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AuctionWeb.Models;
+
+namespace AuctionWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories/ListOfCats
+        public ActionResult ListOfCats()
+        {
+            using (var ctx = new AuctionSiteDBEntities())
+            {
+                var List = ctx.Categories.ToList();
+                return PartialView("ListOfCats", List);
+            }
+        }
+    }
+}
